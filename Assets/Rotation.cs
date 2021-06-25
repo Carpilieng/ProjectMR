b8cb9ddea0d878ea6749e259d9e0ad742b74d26a
JulesLalu <86226740+JulesLalu@users.noreply.github.com>
2021-06-25 17:09:56 +0100

    Update Rotation.cs

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Rotation : MonoBehaviour
 {
-    float speed = 50.0f;
+    Vector3 myVector;
+    public float x_cam, y_cam, z_cam;
+    public float x_cam_1, y_cam_1, z_cam_1;
+    public float angle1, angle2, angle3;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(GameObject.Find("Cube").transform.position);
+        myVector = GameObject.Find("Main Camera").transform.position;
+        x_cam = myVector.x;
+        y_cam = myVector.y;
+        z_cam = myVector.z;
+        angle1 = (float)Math.Atan(y_cam / x_cam);
+        angle2 = (float)Math.Atan(z_cam / x_cam);
+        angle3 = (float)Math.Atan(z_cam / y_cam);
+        transform.Rotate(angle1,angle2,angle3);
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        myVector = GameObject.Find("Main Camera").transform.position;
+        x_cam_1 = myVector.x;
+        y_cam_1 = myVector.y;
+        z_cam_1 = myVector.z;
+        if ((x_cam_1 - x_cam)*(y_cam_1 - y_cam)!=0) {
+            angle1 = (float)Math.Atan((y_cam_1 - y_cam) / (x_cam_1 - x_cam));
+            angle2 = (float)Math.Atan((z_cam_1 - z_cam) / (x_cam_1 - x_cam));
+            angle3 = (float)Math.Atan((z_cam_1 - z_cam) / (y_cam_1 - y_cam));
+        }
+        transform.Rotate(angle1, angle2, angle3);
+        x_cam = myVector.x;
+        y_cam = myVector.y;
+        z_cam = myVector.z;
     }
 }
